85f59060c0fed26b8d2f70b5737b2fa281c055a2
akameco <akameco.t@gmail.com>
2017-08-03 03:48:33 +0900

    Refactor

--- src/index.js
+++ src/index.js
@@ -31,34 +31,83 @@ const getPrefix = ({
 
 const getId = (path, prefix) => {
   if (!(path.isIdentifier() && path.node.name)) {
-    return false
+    throw new Error(`require Object key`)
   }
   return p.join(prefix, path.node.name).replace(/\//g, '.')
 }
 
 const isLiteral = node => t.isStringLiteral(node) || t.isTemplateLiteral(node)
 
-export default function({ types: t }: Object) {
+const isValidate = (path: Object, state: State): boolean => {
+  const callee = path.get('callee')
+  if (!callee.isIdentifier()) {
+    return false
+  }
+
+  if (!isImportLocalName(callee.node.name, state)) {
+    return false
+  }
+
+  const messagesObj = path.get('arguments')[0]
+  if (!messagesObj) {
+    return false
+  }
+
+  if (!(messagesObj.isObjectExpression() || messagesObj.isIdentifier())) {
+    return false
+  }
+
+  return true
+}
+
+const replaceProperties = (properties: Object[], state) => {
+  const prefix = getPrefix(state)
+
+  for (const prop of properties) {
+    const v = prop.get('value')
+
+    // { defaultMessage: 'hello', description: 'this is hello' }
+    if (v.isObjectExpression()) {
+      const objProps = v.get('properties')
+
+      // { id: 'already has id', defaultMessage: 'hello' }
+      const isNotHaveId = objProps.every(v => v.get('key').node.name !== 'id')
+      if (!isNotHaveId) {
+        continue
+      }
+
+      const id = getId(prop.get('key'), prefix)
+
+      v.replaceWith(
+        t.objectExpression([
+          t.objectProperty(t.stringLiteral('id'), t.stringLiteral(id)),
+          ...objProps.map(v => v.node),
+        ])
+      )
+
+      // 'hello' or `hello ${user}`
+    } else if (isLiteral(v)) {
+      const id = getId(prop.get('key'), prefix)
+
+      v.replaceWith(
+        t.objectExpression([
+          t.objectProperty(t.stringLiteral('id'), t.stringLiteral(id)),
+          t.objectProperty(t.stringLiteral('defaultMessage'), v.node),
+        ])
+      )
+    }
+  }
+}
+
+export default function() {
   return {
     visitor: {
       CallExpression(path: Object, state: State) {
-        const callee = path.get('callee')
-        if (!callee.isIdentifier()) {
-          return
-        }
-
-        if (!isImportLocalName(callee.node.name, state)) {
+        if (!isValidate(path, state)) {
           return
         }
 
         const messagesObj = path.get('arguments')[0]
-        if (!messagesObj) {
-          return
-        }
-
-        if (!(messagesObj.isObjectExpression() || messagesObj.isIdentifier())) {
-          return
-        }
 
         let properties
 
@@ -73,48 +122,8 @@ export default function({ types: t }: Object) {
           properties = obj.path.get('init').get('properties')
         }
 
-        if (!properties) {
-          return
-        }
-
-        const prefix = getPrefix(state)
-
-        for (const prop of properties) {
-          const v = prop.get('value')
-
-          // { defaultMessage: 'hello', description: 'this is hello' }
-          if (v.isObjectExpression()) {
-            const objProps = v.get('properties')
-
-            // { id: 'already has id', defaultMessage: 'hello' }
-            const isNotHaveId = objProps.every(
-              v => v.get('key').node.name !== 'id'
-            )
-            if (!isNotHaveId) {
-              continue
-            }
-
-            const id = getId(prop.get('key'), prefix)
-
-            v.replaceWith(
-              t.objectExpression([
-                t.objectProperty(t.stringLiteral('id'), t.stringLiteral(id)),
-                ...objProps.map(v => v.node),
-              ])
-            )
-          }
-
-          // 'hello' or `hello ${user}`
-          if (isLiteral(v)) {
-            const id = getId(prop.get('key'), prefix)
-
-            v.replaceWith(
-              t.objectExpression([
-                t.objectProperty(t.stringLiteral('id'), t.stringLiteral(id)),
-                t.objectProperty(t.stringLiteral('defaultMessage'), v.node),
-              ])
-            )
-          }
+        if (properties) {
+          replaceProperties(properties, state)
         }
       },
     },
