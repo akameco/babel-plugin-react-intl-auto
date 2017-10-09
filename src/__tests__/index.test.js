b09c67f89dd8369573910ed802bff54d53d6c960
akameco <akameco.t@gmail.com>
2017-10-09 19:02:41 +0900

    Fix windows path

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,9 +1,15 @@
 // @flow
-import { resolve } from 'path'
+import path from 'path'
+import slash from 'slash'
 import pluginTester from 'babel-plugin-tester'
 import plugin from '../'
 
-const filename = resolve(__dirname, '../__fixtures__', 'messages.js')
+const filename = path.resolve(
+  slash(__dirname),
+  '..',
+  '__fixtures__',
+  'messages.js'
+)
 
 const basicTest = {
   title: 'basic',
@@ -14,7 +20,7 @@ export default defineMessages({
   hello: 'hello',
   world: 'hello world',
 })
-      `,
+`,
 }
 
 const multiExportTest = {
