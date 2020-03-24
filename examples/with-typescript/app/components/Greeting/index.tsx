2c72c5ce878870d34eb1907d54426b1ccd685425
akameco <akameco.t@gmail.com>
2020-03-24 22:13:49 +0900

    chore: update example

--- examples/with-typescript/app/components/Greeting/index.tsx
+++ examples/with-typescript/app/components/Greeting/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {
   FormattedMessage,
   FormattedNumber,
-  FormattedRelative,
+  FormattedRelativeTime,
 } from 'react-intl'
 import messages from './messages'
 
@@ -16,28 +16,24 @@ interface UserProps {
   user: User
 }
 
-export default class Greeting extends React.Component<UserProps> {
-  render() {
-    const { user } = this.props
-
-    return (
-      <p>
-        <FormattedMessage
-          {...messages.welcome}
-          values={{
-            name: <b>{user.name}</b>,
-            unreadCount: user.unreadCount,
-            formattedUnreadCount: (
-              <b>
-                <FormattedNumber value={user.unreadCount} />
-              </b>
-            ),
-            formattedLastLoginTime: (
-              <FormattedRelative value={user.lastLoginTime} />
-            ),
-          }}
-        />
-      </p>
-    )
-  }
+export default function Greeting({ user }: UserProps) {
+  return (
+    <p>
+      <FormattedMessage
+        {...messages.welcome}
+        values={{
+          name: <b>{user.name}</b>,
+          unreadCount: user.unreadCount,
+          formattedUnreadCount: (
+            <b>
+              <FormattedNumber value={user.unreadCount} />
+            </b>
+          ),
+          formattedLastLoginTime: (
+            <FormattedRelativeTime value={user.lastLoginTime} />
+          ),
+        }}
+      />
+    </p>
+  )
 }
